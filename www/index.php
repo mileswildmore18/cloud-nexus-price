9300d8864796e60695cbd49bfe35b69e46285a88
Miles Wildmore <mileswildmore18@gmail.com>
2024-09-20 11:22:48 -0400

    Changed the colors of the main content of the pricing section to match the required colors for the website. Also adjusted the margin and padding for the information to be level to the center. Also added the color for the background of the section to avoid any white space and to leave the footer and filler content white.

--- www/index.php
+++ www/index.php
@@ -9,68 +9,68 @@
 
 <div class="container-main">
   <!-- Content here -->
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-    />
-    <link
-      href="https://getbootstrap.com/docs/5.3/assets/css/docs.css"
-      rel="stylesheet"
-    />
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <section id="pricing" class="pricing-content section-padding">
-	<div class="container">					
-		<div class="section-title text-center">
-			<h1>Pricing Plan</h1>
-			<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-		</div>					
-		<div class="row text-center">									
-			<div class="col-lg-4 col-sm-4 col-xs-12 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.1s" data-wow-offset="0">
-				<div class="single-pricing">
-					<div class="price-head">								
-						<h2>Starter</h2>
-						<span></span>
-						<span></span>
-						<span></span>
-						<span></span>
-						<span></span>
-						<span></span>
-					</div>
-					<h1 class="price">$600</h1>
-					<h5>Monthly</h5>
-					<ul>
-						<li>5 website</li>
-						<li>50GB Disk Space</li>
-						<li>50 Email Accounts</li>
-					</ul>
-					<a href="#">Get start</a>
-				</div>
-			</div><!--- END COL -->	
-			<div class="col-lg-4 col-sm-4 col-xs-12 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.2s" data-wow-offset="0">
-				<div class="single-pricing">
-					<div class="price-head">								
-						<h2>popular</h2>
-						<span></span>
-						<span></span>
-						<span></span>
-						<span></span>
-						<span></span>
-						<span></span>
-					</div>
-					<h1 class="price">$1100</h1>
-					<h5>Monthly</h5>
-					<ul>
-						<li>10 website</li>
-						<li>50GB Disk Space</li>
-						<li>50 Email Accounts</li>
-						<li>50GB Monthly Bandwidth</li>
-						<li>10 Subdomains</li>
-					</ul>
-					<a href="#">Get start</a>
-				</div>
-			</div><!--- END COL -->		  
-		</div><!--- END ROW -->
-	</div><!--- END CONTAINER -->
-</section>
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+  />
+  <link
+    href="https://getbootstrap.com/docs/5.3/assets/css/docs.css"
+    rel="stylesheet"
+  />
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <section id="pricing" class="pricing-content section-padding">
+	  <div class="container-price">					
+		  <div class="section-title text-center">
+			  <h1 class="price-title">Pricing Plan</h1>
+			  <p class="speech">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+		  </div>					
+		  <div class="row text-center" style="display: flex; justify-content: center;">									
+			  <div class="col-lg-4 col-sm-4 col-xs-12 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.1s" data-wow-offset="0">
+				  <div class="single-pricing">
+					  <div class="price-head">								
+						  <h2>Starter</h2>
+						  <span></span>
+						  <span></span>
+						  <span></span>
+						  <span></span>
+						  <span></span>
+						  <span></span>
+					  </div>
+					  <h1 class="price">$600</h1>
+					  <h5>Monthly</h5>
+					  <ul>
+						  <li>5 website</li>
+						  <li>50GB Disk Space</li>
+						  <li>50 Email Accounts</li>
+					  </ul>
+					  <a href="#">Get started</a>
+				  </div>
+			  </div><!--- END COL -->	
+			  <div class="col-lg-4 col-sm-4 col-xs-12 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.2s" data-wow-offset="0">
+				  <div class="single-pricing">
+					  <div class="price-head">								
+						  <h2>popular</h2>
+						  <span></span>
+						  <span></span>
+						  <span></span>
+						  <span></span>
+						  <span></span>
+						  <span></span>
+					  </div>
+					  <h1 class="price">$1100</h1>
+					  <h5>Monthly</h5>
+					  <ul>
+						  <li>10 website</li>
+						  <li>50GB Disk Space</li>
+						  <li>50 Email Accounts</li>
+						  <li>50GB Monthly Bandwidth</li>
+						  <li>10 Subdomains</li>
+					  </ul>
+					  <a href="#">Get started</a>
+				  </div>
+			  </div><!--- END COL -->		  
+		  </div><!--- END ROW -->
+	  </div><!--- END CONTAINER -->
+  </section>
   
 <?php include_once('./footer.php'); ?>
