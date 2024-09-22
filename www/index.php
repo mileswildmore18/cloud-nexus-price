b59c88152a8fb99d5855b2aeecbb5f5ddbca1a26
Miles Wildmore <mileswildmore18@gmail.com>
2024-09-22 13:39:39 -0400

    Added comments for the CSS file to go into the component CSS file. Also added comments on index php file for clarification where each part begins and ends.

--- www/index.php
+++ www/index.php
@@ -7,6 +7,7 @@
   <p>Filler content</p>
 </div>
 
+<!-- Main container for the pricing section starts here -->
 <div class="container-main">
   <!-- Content here -->
   <link
@@ -18,26 +19,41 @@
     rel="stylesheet"
   />
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  
+  <!-- Main section for the pricing starts here -->
   <section id="pricing" class="pricing-content section-padding">
-	  <div class="container-price">					
-		  <div class="section-title text-center">
+
+    <!-- Container for the pricing starts here -->
+	  <div class="container-price">
+
+      <!-- Title and brief summary of the section -->		
+		  <div class="section-title text-center">  
 			  <h1 class="price-title">Pricing Plan</h1>
 			  <p class="speech">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-		  </div>					
-		  <div class="row text-center" style="display: flex; justify-content: center;">									
+		  </div>
+      <!-- Title and brief summary ends here -->
+
+      <!-- Two column options with prices available starts here -->
+		  <div class="row text-center" style="display: flex; justify-content: center;">
+
+        <!-- First column for starter price starts here -->
 			  <div class="col-lg-4 col-sm-4 col-xs-12 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.1s" data-wow-offset="0">
 				  <div class="single-pricing">
 					  <div class="price-head">								
 						  <h2>Starter</h2>
+              <!-- Adding special span dots below title of 1st column -->
 						  <span></span>
 						  <span></span>
 						  <span></span>
 						  <span></span>
 						  <span></span>
 						  <span></span>
+              <!-- Span dots end -->
 					  </div>
 					  <h1 class="price">$600</h1>
 					  <h5>Monthly</h5>
+
+            <!-- Unordered list added for benefits for option chosen -->
 					  <ul>
 						  <li>5 website</li>
 						  <li>50GB Disk Space</li>
@@ -45,20 +61,26 @@
 					  </ul>
 					  <a href="#">Get started</a>
 				  </div>
-			  </div><!--- END COL -->	
+			  </div>
+        <!--- 1st pricing column ends here -->
+        
+        <!-- Second column for Popular Price starts here -->
 			  <div class="col-lg-4 col-sm-4 col-xs-12 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.2s" data-wow-offset="0">
 				  <div class="single-pricing">
 					  <div class="price-head">								
 						  <h2>popular</h2>
+              <!-- Adding special span dots below title of 1st column -->
 						  <span></span>
 						  <span></span>
 						  <span></span>
 						  <span></span>
 						  <span></span>
 						  <span></span>
+              <!-- Span dots end -->
 					  </div>
 					  <h1 class="price">$1100</h1>
 					  <h5>Monthly</h5>
+            <!-- Unordered list added for benefits for option chosen -->
 					  <ul>
 						  <li>10 website</li>
 						  <li>50GB Disk Space</li>
@@ -66,11 +88,18 @@
 						  <li>50GB Monthly Bandwidth</li>
 						  <li>10 Subdomains</li>
 					  </ul>
+            <!-- Unordered list ends here -->
+
 					  <a href="#">Get started</a>
 				  </div>
-			  </div><!--- END COL -->		  
-		  </div><!--- END ROW -->
-	  </div><!--- END CONTAINER -->
+			  </div>
+        <!--- Second pricing column ends here -->		  
+		  </div>
+      <!--- Properties for the 2 columns ends here -->
+	  </div>
+    <!--- Container for the prices ends here -->
   </section>
-  
+  <!-- Pricing section ends here -->
+</div>
+  <!-- Main container for the pricing section ends here -->
 <?php include_once('./footer.php'); ?>
